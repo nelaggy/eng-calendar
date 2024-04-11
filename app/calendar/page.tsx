d6abcabf27472dadd85e54987cf2bc70fb74c51f
Galen Lee <qgl20@cam.ac.uk>
2024-04-11 17:39:44 +0100

    Added page titles - Added titles to calendar and login pages - Moved login button into component file

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -2,10 +2,15 @@ import CalLink from "@/lib/components/cal_link"
 import Calendars from "@/lib/components/calendars"
 import Logout from "@/lib/components/logout_button"
 import { getGroups } from "@/lib/server/fetch"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Calendar'
+}
 
 export default async function Page() {
     const groups = await getGroups()
-    return ( 
+    return (
         <div className='m-4'>
             <div className='text-6xl text-center'>Calendar</div>
             <Calendars cals={groups}>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,46 +1,16 @@
-'use client'
-
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { createClient } from '@/utils/supabase/client'
 import { useRouter } from 'next/navigation'
 
 import type { Database } from '@/lib/supabase'
+import { Metadata } from 'next'
+import Login from '@/lib/components/login_button'
 
-export default function Login() {
-  const router = useRouter()
-  const supabase = createClientComponentClient<Database>()
+export const metadata: Metadata = {
+  title: 'Login'
+}
 
-  const getURL = () => {
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      'http://localhost:3000/';
-  
-    // Make sure to include https:// when not localhost.
-    url = url.includes('http') ? url : `https://${url}`;
-    // Make sure to including trailing /.
-    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-    return url;
-  };
-
-  const handleSignIn = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'google', 
-      options: {
-        queryParams: {
-          access_type: 'offline',
-          prompt: 'consent',
-          hd: 'cam.ac.uk'
-        },
-        redirectTo: `${getURL()}auth/callback`
-      },
-    })
-    router.refresh()
-  }
-
-  return (
-    <>
-      <button onClick={handleSignIn}>Sign in</button>
-    </>
+export default function Page() {
+  return(
+    <Login />
   )
 }
