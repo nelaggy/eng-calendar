26530102776cae0365706bdb7452f36f73c01052
Nelaggy <nelaggy@gmail.com>
2024-11-01 22:36:42 +0000

    Center text on login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="flex flex-col justify-center items-center align-middle space-y-6 h-screen">
+    <div className="flex flex-col justify-center items-center align-middle text-center mx-4 space-y-6 h-screen">
       <h1 className="text-4xl pb-4">Cambridge Engineering Calendar</h1>
       <LoginButton />
       <p>Encountered issues? <a href="https://github.com/nelaggy/eng-calendar/issues" className="underline">Contact</a></p>
